03335c27ec195b753669d47299e2caea6350c219
tambegaurav <gauravkishortambe@gmail.com>
2020-11-19 17:19:18 +0530

    initialized app

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Meg-Events</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
